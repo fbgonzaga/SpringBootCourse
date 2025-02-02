1c626d12ce83bf63c16d4251ba7157479e1fc428
Flavio Gonzaga <fbgonzaga@gmail.com>
2025-02-02 17:35:08 -0300

    inclusao de mais alguns códigos-fonte. Os últimos relacionados a spring security.

---
+++ SpringCore/lesson34_constructorInjection/springcoredemoapp/src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,22 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    //If you only have one constructor then @Autowired on it is optional
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
